396f1d209f8696b09b3e29167245839d9fc1bdf7
Mattias Svala <thebrasse@brasse.org>
2011-02-17 15:36:47 +0100

    Use a ListPreference to select source for QR data.

--- src/com/github/qrgen/WidgetConfigure.java
+++ src/com/github/qrgen/WidgetConfigure.java
@@ -1,6 +1,7 @@
 package com.github.qrgen;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.util.Log;
@@ -9,19 +10,35 @@ public class WidgetConfigure extends PreferenceActivity {
 
     private static final String TAG = "qrgen";
 
+    private Preference manualData;
+    private Preference contactData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.widget_configure);
-        Preference manual = findPreference("qrManual");
-        Preference contact = findPreference("qrContact");
-        manual.setOnPreferenceChangeListener(
+        manualData = findPreference("qrManualData");
+        contactData = findPreference("qrContactData");
+        ListPreference source = (ListPreference)findPreference("qrSource");
+        source.setOnPreferenceChangeListener(
                 new Preference.OnPreferenceChangeListener() {
                     public boolean onPreferenceChange(Preference p, Object o) {
-                        Log.i(TAG, "change: " + o);
+                        updateSource((String)o);
                         return true;
                     }
                 });
+        updateSource(source.getValue());
+    }
+
+    private void updateSource(String source) {
+        Log.i(TAG, "update: " + source);
+        if (source.equals("sourceManual")) {
+            manualData.setEnabled(true);
+            contactData.setEnabled(false);
+        } else {
+            manualData.setEnabled(false);
+            contactData.setEnabled(true);
+        }
     }
 
 }
